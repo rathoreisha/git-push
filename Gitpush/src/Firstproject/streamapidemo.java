cf9a8c95e1a0cc94e66b611a616907dcb0f16af6
rathoreisha <isharathore345@gmail.com>
2022-11-30 16:41:40 +0530

    "my first stream api"

---
+++ Gitpush/src/Firstproject/streamapidemo.java
@@ -0,0 +1,10 @@
+package Firstproject;
+
+public class streamapidemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("ISha");
+	}
+
+}
---
+++ Gitpush/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Smart Computer
+ *
+ */
+module Gitpush {
+}
